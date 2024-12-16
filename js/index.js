b989288fe45276545c1f4731f28fe19cd50b44c0
lodacke <lo.dacke@mail.com>
2024-12-16 14:19:58 +0100

    added nav-logic

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+import { nav } from "/utilities/variables.js"
+
 function renderNav(){
-    
+    nav.innerHTML = `
+    <div class="nav-darkmode"></div>
+    <div class="nav-sunset"></div>
+    <div class="nav-sound"></div>
+    `;
+
 }
+
+renderNav();
---
+++ utilities/variables.js
@@ -0,0 +1,2 @@
+export const main = document.querySelector("main");
+export const nav = document.querySelector("nav");
