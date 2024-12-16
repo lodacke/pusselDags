a7e9fc40f7352b36d2c296faa9ce3ff16e2550ea
lodacke <lo.dacke@mail.com>
2024-12-16 15:37:25 +0100

    added API and theme in it

---
+++ API/characters.js
@@ -0,0 +1,14 @@
+export const themes = [
+    {
+        name: "babblarna",
+        img: ""
+    },
+    {
+        name: "",
+        img: ""
+    },
+    {
+        name: "babblarna",
+        img: ""
+    }
+];
